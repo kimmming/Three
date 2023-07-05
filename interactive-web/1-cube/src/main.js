8880b5b4f1fec5548eb6ef06317516f319fd7224
min <rlaalswl1238@naver.com>
2023-07-05 23:37:59 +0900

    Feat: #3 다면체로 변경하기

--- interactive-web/1-cube/src/main.js
+++ interactive-web/1-cube/src/main.js
@@ -24,9 +24,10 @@ function init(){
     500,
   );
 
-  const geometry = new THREE.BoxGeometry(2,2,2);
-  const material = new THREE.MeshStandardMaterial({
-    color:'skyblue',
+  const cubeGeometry = new THREE.IcosahedronGeometry(1);
+  const cubeMaterial = new THREE.MeshLambertMaterial({
+    color:'pink',
+    emissive: 'black',
     // transparent: true,
     // opacity:0.5,
     // visible:false,
@@ -34,31 +35,37 @@ function init(){
     // side: THREE.BackSide
   })
 
-  const cube = new THREE.Mesh(geometry, material);
-
-  scene.add(cube);
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  const skeletonGeometry = new THREE.IcosahedronGeometry(2);
+  const skeletonMaterial = new THREE.MeshBasicMaterial({
+    wireframe: true,
+    transparent: true,
+    opacity:0.2,
+    color: 0xaaaaaa
+  }
+  );
+  const skeleton = new THREE.Mesh(skeletonGeometry,skeletonMaterial)
   
-  // camera.position.z =5;
-  camera.position.set(3,4,5);
+  scene.add(cube, skeleton);
+  camera.position.set(0,0,5);
 
-  camera.lookAt(cube.position);
-
-  const directionalLight = new THREE.DirectionalLight(0xf0f0f0, 1);
-  directionalLight.position.set(-1,2,3);
+  const directionalLight = new THREE.DirectionalLight('white', 1);
   scene.add(directionalLight);
 
-  const ambientLight = new THREE.AmbientLight(0xffffff,0.1);
-  ambientLight.position.set(3,2,1);
-  scene.add(ambientLight)
 
   const clock = new THREE.Clock();
 
   render();
 
   function render(){
-    cube.rotation.x = clock.getDelta();
-    // cube.position.y = Math.sin(cube.rotation.x);
-    // cube.scale.x = Math.cos(cube.rotation.x);
+
+    const elapsedTime = clock.getElapsedTime();
+    cube.rotation.x = elapsedTime;
+    cube.rotation.y = elapsedTime;
+
+    skeleton.rotation.x = elapsedTime * 1.5;
+    skeleton.rotation.y = elapsedTime * 1.5;
+
     renderer.render(scene, camera);
     requestAnimationFrame(render);
   };
