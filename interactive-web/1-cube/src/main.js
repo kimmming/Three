46e402ff15130257d8812cf5be6dd4705d529916
min <rlaalswl1238@naver.com>
2023-07-05 21:21:43 +0900

    ✨ Feat : # Box geometry 추가

--- interactive-web/1-cube/src/main.js
+++ interactive-web/1-cube/src/main.js
@@ -5,18 +5,24 @@ window.addEventListener('load', function(){
 })
 
 function init(){
-  const renderer = new THREE.WebGL1Renderer();
+  const renderer = new THREE.WebGL1Renderer({
+    // alpha: true,
+  });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
   document.body.appendChild(renderer.domElement);
 
-  const scene = new THREE.Scene()
+  const scene = new THREE.Scene();
 
   const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth/window.innerHeight,
     1,
     500,
-  )
+  );
+
+  const geometry = new THREE.BoxGeometry(1,1,1);
 
   renderer.render(scene, camera)
-}
+};
