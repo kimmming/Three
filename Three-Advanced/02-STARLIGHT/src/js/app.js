456a617e6cea3ac5dddf50ca35ce2d30ac03d70c
min <rlaalswl1238@naver.com>
2023-08-02 18:47:16 +0900

    feat: #5 한국에 point 추가 및 함수 분리

--- Three-Advanced/02-STARLIGHT/src/js/app.js
+++ Three-Advanced/02-STARLIGHT/src/js/app.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-
+import { convertLatLngToPos } from './utils';
 export default function () {
   const renderer = new THREE.WebGLRenderer({
     alpha: true,
@@ -61,6 +61,8 @@ export default function () {
     const geometry = new THREE.SphereGeometry(1.3, 30,30);
 
     const mesh = new THREE.Mesh(geometry, material);
+    mesh.rotation.y = -Math.PI/ 2
+
     return mesh;
   };
   
@@ -74,6 +76,8 @@ export default function () {
     const geometry = new THREE.SphereGeometry(1.5, 30,30);
 
     const mesh = new THREE.Mesh(geometry, material);
+    mesh.rotation.y = -Math.PI/ 2;
+
     return mesh;
 
   }
@@ -105,11 +109,30 @@ export default function () {
     return star;
   }
 
+  const createPoint = () =>{
+    const point={
+      lat:37.56668* (Math.PI/180),
+      lng:126.97841* (Math.PI/180),
+    }
+
+    const position = convertLatLngToPos(point,1.3);
+
+    const mesh = new THREE.Mesh(
+      new THREE.SphereGeometry(0.03,20,20),
+      new THREE.MeshBasicMaterial({color:0xff0000})
+    );
+
+    mesh.position.set(position.x,position.y,position.z);
+    return mesh;
+  }
+
+
   const create =()=>{
     const earth1 = createEarth1();
     const earth2 = createEarth2();
     const star = createStar();
-    scene.add(earth1,earth2,star);
+    const point = createPoint();
+    scene.add(earth1,earth2,star,point);
 
     return{
       earth1,
@@ -136,14 +159,14 @@ export default function () {
 
   const draw = (obj) => {
     const {earth1, earth2, star} = obj;
-    earth1.rotation.x += 0.0005;
-    earth1.rotation.y += 0.0005;
+    // earth1.rotation.x += 0.0005;
+    // earth1.rotation.y += 0.0005;
 
-    earth2.rotation.x += 0.0005;
-    earth2.rotation.y += 0.0005;
+    // earth2.rotation.x += 0.0005;
+    // earth2.rotation.y += 0.0005;
 
-    star.rotation.x +=0.0001;
-    star.rotation.y +=0.0001;
+    // star.rotation.x +=0.0001;
+    // star.rotation.y +=0.0001;
 
     controls.update();
     renderer.render(scene, camera);
