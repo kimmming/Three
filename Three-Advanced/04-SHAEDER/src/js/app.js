e42105591cfa68a07d4e47467433b440cb39587f
min <rlaalswl1238@naver.com>
2023-08-09 00:13:29 +0900

    feat: #7 GLSL Shader-1

--- Three-Advanced/04-SHAEDER/src/js/app.js
+++ Three-Advanced/04-SHAEDER/src/js/app.js
@@ -9,4 +9,51 @@ export default function () {
 
   const gl = canvas.getContext('webgl');
   
+  // vertex shader 만들기
+  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, `
+    attribute vec2 position;
+    varying vec2 vPosition;
+
+    void main(){
+        vec2 newPosition = (position + 1.0) / 2.0;
+        gl_Position = vec4(position, 0.0, 1.0);
+
+        vPosition = newPosition;
+      }
+  `);
+  gl.compileShader(vertexShader);
+
+  // fragment shader 만들기
+  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, `
+    precision mediump float;
+    varying vec2 vPosition;
+      
+    void main(){
+      gl_FragColor = vec4(vPosition, 1.0, 0.3);
+    }
+  `);
+  gl.compileShader(fragmentShader);
+
+  // vertex & fragment 를 하나로
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+
+  gl.linkProgram(program);
+  gl.useProgram(program);
+
+  // 정점 데이텉 넘겨주기
+  const vertices = new Float32Array([-1,-1,-1,1,1,1,-1,-1,1,1,1,-1]);
+  const vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+  // 정점의 위치를 어떻게 계산할지 정보를 넘겨주기
+  const position = gl.getAttribLocation(program, 'position');
+  gl.vertexAttribPointer(position, 2, gl.FLOAT, false, 0, 0);  
+
+  gl.enableVertexAttribArray(position);
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
 }
