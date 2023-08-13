2ba20fe3fafc25da23144844ccad859ab0083cb5
min <rlaalswl1238@naver.com>
2023-08-13 19:56:48 +0900

    feat: #9 카드 모양 만들기

--- interactive-web/3-interactive-card/src/main.js
+++ interactive-web/3-interactive-card/src/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import { GUI } from 'lil-gui'
 import Card from './Card.js'
 
 window.addEventListener('load', function(){
@@ -7,6 +8,7 @@ window.addEventListener('load', function(){
 })
 
 function init(){
+  const gui = new  GUI();
 
   const renderer = new THREE.WebGL1Renderer({
     antialias: true,
@@ -34,14 +36,40 @@ function init(){
   const card = new Card({
     width: 10,
     height: 15.8,
+    radius:0.5,
     color: '#0077ff',
   });
 
   scene.add(card.mesh);
 
+  const cardFolder = gui.addFolder('Card');
+
+  cardFolder
+    .add(card.mesh.material, 'roughness')
+    .min(0)
+    .max(1)
+    .step(0.01)
+    .name('material.roughness');
+
+  cardFolder
+  .add(card.mesh.material, 'metalness')
+  .min(0)
+  .max(1)
+  .step(0.01)
+  .name('material.metalness');
+
+
+
   const ambientLight = new THREE.AmbientLight(0xffff, 0.8);
   scene.add(ambientLight);
-  
+
+  const directionalLight1 = new THREE.DirectionalLight(0xffffff, 0.6);
+  const directionalLight2 = directionalLight1.clone();
+
+  directionalLight1.position.set(1,1,3);
+  directionalLight2.position.set(-1,1,-3);
+
+  scene.add(directionalLight1, directionalLight2);
 
   render();
 
